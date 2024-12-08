731cecb9c3cd386624582be4d59d592541f295f6
shreejana maharjan <shreejana.maharjan61@gmail.com>
2024-12-08 14:41:52 +0545

    navbar responsive added

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'block';
+}
+function closeSidebar() {
+    const closebar = document.querySelector('.sidebar');
+    closebar.style.display = 'none';
+}
